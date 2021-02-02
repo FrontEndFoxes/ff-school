ace6998a1ca506716c4b3d80e9c22d82cdb074b7
Jen Looper <jen.looper@gmail.com>
2021-02-02 10:27:26 -0500

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,260 @@
+<template>
+  <nav id="header" class="nav fixed w-full z-30 top-0 text-white">
+    <div class="w-full mx-auto flex flex-wrap items-center justify-between">
+      <!-- App bar -->
+      <div class="flex items-center pl-4">
+        <a
+          class="p-2 text-white no-underline hover:no-underline text-2xl lg:text-2xl"
+          href="/"
+        >
+          <img class="pull-left pt-1" src="../assets/logo-sm.png" alt="" />
+          <span class="pt-5 pl-3">Front-End Foxes School</span>
+        </a>
+      </div>
+    </div>
+  </nav>
+
+  <main class="relative flex-grow">
+    <div class="pt-24">
+      <div class="container mx-auto px-10 flex flex-wrap flex-col md:flex-row">
+        <!--Left Col-->
+        <div class="flex flex-col w-full md:w-3/5 text-center md:text-left">
+          <h1 class="my-4 text-5xl font-bold leading-tight">
+            A New Kind of Bootcamp
+          </h1>
+          <p class="leading-normal text-2xl md:text-4xl mb-8">
+            For women, by women. Grow your front-end development skills with us!
+            Stay informed as we open our first cohort in the Spring of 2021.
+          </p>
+
+          <div
+            class="mb-5 flex flex-wrap lg:flex lg:flex-no-wrap md:flex lg:flex-no-wrap w-full justify-center md:justify-start z-10"
+          >
+            <a
+              class="btn w-full lg:w-6/12 text-white mt-6 rounded-lg overflow-hidden py-3 bg-secondary-500 hover:bg-secondary-400"
+              target="_blank"
+              href="http://eepurl.com/gdH21v"
+              ><div class="uppercase text-center text-lg">Keep me in the loop!</div></a
+            >
+          </div>
+        </div>
+        <!--Right Col-->
+        <div class="w-full md:w-2/5 py-6 text-center">
+          <img class="logo w-full z-50" src="../assets/logo.svg" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <div class="relative -mt-12 lg:-mt-24">
+      <svg
+        viewBox="0 0 1428 174"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g
+            transform="translate(-2.000000, 44.000000)"
+            fill="#f7fafc"
+            fill-rule="nonzero"
+          >
+            <path
+              d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496"
+              opacity="0.100000001"
+            />
+            <path
+              d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
+              opacity="0.100000001"
+            />
+            <path
+              d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z"
+              id="Path-4"
+              opacity="0.200000003"
+            />
+          </g>
+          <g
+            transform="translate(-4.000000, 76.000000)"
+            fill="#f7fafc"
+            fill-rule="nonzero"
+          >
+            <path
+              d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"
+            />
+          </g>
+        </g>
+      </svg>
+    </div>
+  </main>
+
+  <section class="bg-gray-100 py-8">
+    <div class="container mx-auto flex flex-wrap pt-4">
+      <h2
+        class="w-full text-3xl md:text-5xl font-bold leading-tight text-center text-gray-800"
+      >
+        Women Teaching Women
+      </h2>
+      <div class="w-full mb-4">
+        <div
+          class="h-1 mx-auto gradient w-64 opacity-25 my-3 py-0 rounded-t"
+        ></div>
+      </div>
+
+      <div class="lg:flex">
+        <div class="lg:w-1/3 p-2">
+          <div class="p-2">
+            <img
+              class="mentoring"
+              src="../assets/mentoring.jpg"
+              alt="mentoring"
+            />
+          </div>
+        </div>
+        <div class="lg:w-1/3 p-2">
+          <div class="p-2">
+            <img
+              class="mentoring"
+              src="../assets/mentoring2.jpg"
+              alt="mentoring"
+            />
+          </div>
+        </div>
+        <div class="lg:w-1/3 p-2">
+          <div class="p-2">
+            <img
+              class="mentoring"
+              src="../assets/mentoring3.jpg"
+              alt="mentoring"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="footer">
+    <div class="mx-auto flex items-center">
+      <div class="w-full mx-auto flex flex-wrap items-center">
+        <div
+          class="hidden md:flex w-full md:w-1/2 justify-center md:justify-start text-white"
+        >
+          <ul
+            class="list-reset w-1/2 lg:flex justify-start flex-1 items-center"
+          >
+            <li class="mr-3"></li>
+            <li class="mr-3">
+              <a
+                href="mailto:info@frontendfoxes.org"
+                class="hover:text-orange-600 uppercase inline-block py-2 px-4 text-white no-underline"
+              >
+                Contact Us
+              </a>
+            </li>
+
+            <li class="mr-3">
+              <span class="hover:text-orange-600">
+                A 501(c)(3) Public Charity
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div
+          class="flex w-full pt-2 content-center justify-end md:w-1/2 md:justify-end"
+        >
+          <ul class="list-reset flex">
+            <li class="mr-3">
+              <a
+                class="uppercase inline-block py-2 px-4 text-white no-underline"
+                href="https://twitter.com/frontendfoxes"
+                aria-label="Twitter"
+                target="_blank"
+                rel="noopener"
+              >
+                <i
+                  class="text-white fa fa-twitter fa-2x"
+                  aria-hidden="true"
+                ></i>
+              </a>
+            </li>
+            <!--<li class="mr-3">
+              <a
+                class="uppercase inline-block py-2 px-4 text-white no-underline"
+                href="https://communityinviter.com/apps/vuevixens/front-end-foxes"
+                aria-label="Slack"
+                target="_blank"
+                rel="noopener"
+              >
+                <i class="text-white fa fa-slack fa-2x" aria-hidden="true"></i>
+              </a>
+            </li>-->
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    drawer() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+  watch: {
+    isOpen: {
+      immediate: true,
+      handler(isOpen) {
+        if (process.client) {
+          if (isOpen) document.body.style.setProperty("overflow", "hidden");
+          else document.body.style.removeProperty("overflow");
+        }
+      },
+    },
+  },
+  mounted() {
+    document.addEventListener("keydown", (e) => {
+      if (e.keyCode == 27 && this.isOpen) this.isOpen = false;
+    });
+  },
+};
+</script>
+<style>
+.gradient {
+  background: linear-gradient(
+    90deg,
+    theme("colors.primary.700") 0%,
+    theme("colors.primary.500") 100%
+  );
+}
+
+.nav,
+.footer {
+  background-color: theme("colors.primary.700");
+}
+
+.mentoring {
+  filter: grayscale(100%);
+}
+
+.button {
+  background: theme("colors.secondary.500");
+  color: theme("colors.white");
+}
+.button:hover {
+  background: theme("colors.secondary.400");
+}
+
+.logo {
+  max-width: 500px;
+}
+
+#mc-embedded-subscribe {
+  margin: 0;
+  border: 0px;
+}
+</style>
