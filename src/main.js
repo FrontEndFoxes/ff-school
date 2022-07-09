808467a48f8d5a4ee3c483bad5627885f2b16d97
Jen Looper <jen.looper@gmail.com>
2022-07-09 17:19:53 -0400

    cohort open

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './index.css';
 
 let app = createApp(App)
 //form status and url set here
-app.config.globalProperties.regFormOpen = false;
-app.config.globalProperties.formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSe09HSqhJE9mAf_x26J4SFeTITF848nqaTmz-kGoVhMULVqCA/viewform"
+app.config.globalProperties.regFormOpen = true;
+app.config.globalProperties.formUrl = "https://forms.gle/odtFXjx7xtU78JH5A"
 app.config.globalProperties.status = "Our C3 #RedFox cohort, sponsored by Reddit, will start on Sept 19th, 2022."
 app.use(router).mount('#app')
