0058bdb14b84edb5f82ca2cf15ee0b2711e4fa22
Barbara <basborges@gmail.com>
2022-09-27 17:28:51 +0200

    allow running the project without env

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
 
-export default {
-	plugins: [vue()],
-};
+export default defineConfig({
+  plugins: [vue()],
+  define: {
+    'process.env': {}
+  }
+})
